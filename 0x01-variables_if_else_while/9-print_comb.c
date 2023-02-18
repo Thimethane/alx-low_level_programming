4670e077a08020f0e795a04e512bc9258d4aa3f8
thimethane <thimethane@gmail.com>
2023-02-18 06:12:52 -0800

    Update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,8 +9,15 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
+	{
 		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
 
 	putchar('\n');
 

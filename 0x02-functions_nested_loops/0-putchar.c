8fad02d178025b9c4ddac83afe7a0a1e93b1d43c
thimethane <thimethane@gmail.com>
2023-02-21 06:29:48 -0800

    Functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,14 +11,4 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-int main (void)
-{
-	char word[8] = "_putchar";
-	int i;
 
-	for (i = 0; i < 9; i++)
-		_putchar(word[i]);
-	_putchar('\n');
-
-	return (0);
-}

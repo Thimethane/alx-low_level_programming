0c4e7e8fd303a44788b7fa63efb6cbf22ff0ce08
thimethane <thimethane@gmail.com>
2023-03-15 04:27:34 -0700

    #include main.h

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,27 +2,38 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/**
+ * main - Entry point. Adds positive numbers passed as arguments.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: 0 if successful, 1 if any argument contains non-digit characters.
+ */
 int main(int argc, char *argv[])
 {
-    int sum = 0;
+	int sum = 0;
+	int i, j;
 
-    if (argc < 2) {
-        printf("0\n");
-        return 0;
-    }
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    for (int i = 1; i < argc; i++) {
-        int j = 0;
-        while (argv[i][j]) {
-            if (!isdigit(argv[i][j])) {
-                printf("Error\n");
-                return 1;
-            }
-            j++;
-        }
-        sum += atoi(argv[i]);
-    }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
 
-    printf("%d\n", sum);
-    return 0;
+	printf("%d\n", sum);
+
+	return (0);
 }

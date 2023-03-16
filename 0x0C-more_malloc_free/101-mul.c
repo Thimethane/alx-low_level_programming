39672357b16533e8c25bbef062f2fd766f49d0f3
thimethane <thimethane@gmail.com>
2023-03-16 05:56:06 -0700

    More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,28 +1,34 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - Check the code
+ * main - Multiplies two positive numbers.
  * @argc: number of arguments
  * @argv: arguments
- * Return: Nothing
+ * Return: 0 on success, 98 on failure
  */
 int main(int argc, char **argv)
 {
-	int num1, num2, result;
+    int num1, num2, result;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	else
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		result = num1 * num2;
-		printf("%d\n", result);
-		return (0);
-	}
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return (98);
+    }
+    else
+    {
+        num1 = atoi(argv[1]);
+        num2 = atoi(argv[2]);
+
+        if (num1 <= 0 || num2 <= 0)
+        {
+            printf("Error\n");
+            return (98);
+        }
+
+        result = num1 * num2;
+        printf("%d\n", result);
+        return (0);
+    }
 }

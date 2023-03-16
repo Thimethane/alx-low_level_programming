8b78b7e2a30d834c18693ffcb94841abd7a94e14
thimethane <thimethane@gmail.com>
2023-03-16 06:41:21 -0700

    Connect

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,34 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - Multiplies two positive numbers.
- * @argc: number of arguments
- * @argv: arguments
- * Return: 0 on success, 98 on failure
+ * multiply - Multiplies two positive integers and prints the result
+ * @num1: The first integer
+ * @num2: The second integer
+ *
+ * Return: 0 if successful
  */
-int main(int argc, char **argv)
+int multiply(int num1, int num2) 
 {
-    int num1, num2, result;
+    int result = num1 * num2;
+    printf("%d\n", result);
+    return 0;
+}
 
-    if (argc != 3)
+/**
+ * is_number - Checks if a string contains only digits
+ * @str: The string to check
+ * 
+ * Return: 1 if the string contains only digits, 0 otherwise
+ */
+int is_number(char *str) 
+{
+    int i = 0;
+    while (str[i] != '\0') 
     {
-        printf("Error\n");
-        return (98);
-    }
-    else
-    {
-        num1 = atoi(argv[1]);
-        num2 = atoi(argv[2]);
-
-        if (num1 <= 0 || num2 <= 0)
+        if (!isdigit(str[i])) 
         {
-            printf("Error\n");
-            return (98);
+            return 0;
         }
-
-        result = num1 * num2;
-        printf("%d\n", result);
-        return (0);
+        i++;
     }
+    return 1;
+}
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Arguments array
+ * 
+ * Return: 0 if successful, 1 if an error occurs
+ */
+int main(int argc, char *argv[])
+{
+    int num1, num2;
+
+    if (argc != 3) 
+    {
+        fprintf(stderr, "Usage: %s <num1> <num2>\n", argv[0]);
+        return 1;
+    }
+
+    if (!is_number(argv[1]) || !is_number(argv[2])) 
+    {
+        fprintf(stderr, "Error: Arguments must be integers\n");
+        return 1;
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+
+    multiply(num1, num2);
+
+    return 0;
 }

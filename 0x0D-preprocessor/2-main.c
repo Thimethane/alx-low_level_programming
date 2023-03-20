7402ef2a0e07251bfc03c608f52d3b59d5e9b728
thimethane <thimethane@gmail.com>
2023-03-20 06:30:08 -0700

    C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ * main - Prints the name of file
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+
+	return (0);
+}
